bf2488dce2acc9fee5ca1f317a4b88e9bab3e0ce
king <wangzhongwen@nowunc.com>
2020-11-06 15:35:51 +0800

    feat: add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
